63438e3142e9972c79fba5f1e8fff3c401f19ace
Maxi M <maximilianeias.meier@web.de>
2022-02-03 22:04:27 +0100

    Bot can ping #feature

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
+import discord
 import discord as dc
+from discord.ext import commands
+
+targetPlayerName = ""
+targetGameName = ""
+
 
 def getBotToken():
     return open("E:\Öffentliche Dokumente\BanBot-Verification\keys.txt").read()
+
 
 intent = dc.Intents.default()
 intent.members = True
 
 TOKEN = getBotToken()
 
-client = dc.Client()
+bot = commands.Bot(command_prefix="$")
+client = discord.Client()
 
 print("Booting..")
 
 
 @client.event
 async def on_ready():
-    print("Bot is now online")
+    print("Client-Bot is now online!")
 
 
+@bot.event
+async def on_ready():
+    print("Command-Bot is now online!")
 
+
+@bot.command()
+async def ping(ctx):
+    await ctx.channel.send("pong")
+
+
+bot.run(TOKEN)
 client.run(TOKEN)
